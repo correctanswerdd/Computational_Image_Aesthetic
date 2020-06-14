34a481760857733d7be9c4aa615ac9265bf8f73f
DUNBUJODA <51895263+DUNBUJODA@users.noreply.github.com>
2020-06-14 19:36:25 +0800

    Update cal_distribution.py

--- Skill-MTCNN/cal_distribution.py
+++ Skill-MTCNN/cal_distribution.py
@@ -6,7 +6,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 def main(_):
     net = Network(input_size=(227, 227, 3),
                   output_size=24)
-    net.cal_distribution(model_path='./model_MTCNN/', image_path='3.jpg')
+    net.cal_distribution(model_path='./model_MTCNN/', image_path='9.jpeg')
 
 if __name__ == '__main__':
     tf.app.run()
